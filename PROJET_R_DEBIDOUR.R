50ae2402cdb302d0b992acb0846234bc16ba01d8
Julien Debidourjdebidour <jdebidour@ensc.fr>
2023-05-18 05:20:35 +0200

    stats_descriptive_inférentielle_ACP

--- PROJET_R_DEBIDOUR.R
+++ PROJET_R_DEBIDOUR.R
@@ -586,6 +586,207 @@ summary(lm_model)
 chisq.test(donneesinferentielle$Regional.indicator, donneesinferentielle$Perceptions.of.corruption)
 
 
+          #### FONCTION GRAPHIQUE DENSITÉ ÉCHANTILLON DE CHAQUE VARIABLE #### 
+
+donnees<-read.csv2("/Users/debidour/Desktop/ENSC/PERSONNEL/1A/MATHS/Projet R/Data.csv",header=TRUE,dec=".",sep=",")
+
+# Voici une fonction permettant d'estimer et de représenter graphiquement
+# la densité d'un échantillon (de manière plus "lisse" et plus "jolie") qu'un histogramme.
+# NB : il s'agit d'un estimateur (nonparamétrique) à noyau de la densité (kernel estimator, en anglais)
+# pour lequel il faut gérer un paramêtre "bandwidth" correspondant à la largeur de fenêtre :
+# - plus la largeur de fenêtre est grande, plus l'estimateur sera "lisse" et va donc "écraser" 
+# l'information,
+# - plus la largeur de fenêtre est petite, moins l'estimateur sera "lisse" et fera un "pic" 
+# uniquement là où il y a une observation.
+# ==> il faut donc calibrer correctement ce paramêtre "width". 
+#     Une valeur raisonnable est proposée par défaut.
+
+Graphdensite.prog <- function(x,bandwidth=2*(summary(x)[5]-summary(x)[2])){
+  plot(density(x,width= bandwidth),type="l",xlab="x",
+       ylab=" ",main=" ",xlim=c(min(x)-sd(x),max(x)+sd(x)))
+  title(paste("Estimation à noyau de la densité avec une largeur de fenêtre = ",bandwidth),
+        cex.main=0.8)
+}
+
+Graphdensite.prog(joie)   # une représentation graphique plus jolie de la densité de l'échantillon
+abline(v=990,col=2,lwd=2)  # affichage en rouge de la valeur obtenue par Cornu
+abline(v=mean(ech),col=3,lwd=2) # affichage en vert de la valeur moyenne issue des expériences de Michelson
+
+          #### TEST DE NORMALITÉ DE CHAQUE VARIABLE #### 
+
+      ## TEST POUR LADDER SCORE ##
+
+  ## test de Shapiro-Wilk ##
+
+shapiro.test(joie)
+
+  ## test de Kolmogorov ##
+
+ks.test(joie,"pnorm",mean(ech),sqrt(var(joie)))
+
+# Pour ces deux tests, l'hypothèse nulle est H0 : l'échantillon X1,...,X20 suit une loi normale.
+# et l'hypothèse alternative est H1 : non H0 (c'est à dire l'échantillon ne suit pas une loi normale). 
+#
+# IMPORTANT : règle universelle d'interprétation des sorties d'un test d'hypothèses
+#          Il faut regarder la p-value du test et la comparer au risque de première espèce (alpha=Proba
+#          de rejeter H0 a tort) que l'utilisateur se donne (par exemple, alpha=5%)
+#          Si p-value<alpha, alors on rejette H0 et on retient H1.
+#          Si p-value>alpha, alors on ne peut pas rejeter H0 et on conserve donc HO (parfois par défaut).
+#
+# Commentaires des sorties des tests :
+# Pour les deux tests, on a des p-values supérieures à alpha=5% (p-value pour Shapiro-Wilk=9.88%>5%)
+# et p-value pour SKolmogorov=54,1%>5%), on ne peut donc pas rejeter H0. 
+# En conclusion, on accepte que notre échantillon de travail provient bien d'une loi normale,
+# et on peut donc mettre en oeuvre sereinement le test de Student.
+#
+# NB : théoriquement, le test de Kolmogorov a été developpé pour des variables aléatoires absolument
+# continues et donc pour lesquelles la probabilité d'observer deux fois la même valeur est nulle.
+# Mais ceci peut arriver dans la realité lorsque l'on dispose d'un échantillon d'observations.
+# Le "Warning message" indique juste qu'il y a des valeurs égales dans l'échantillon, mais la fonction sait
+# bien gérée ce cas pratique !
+# En effet, si on bruite très légèrement l'échantillon pour ne plus avoir de valeurs égales, 
+# la p-value du test et la valeur de la statistique de test D ne vont pas être tres différentes :
+
+ks.test(ech+rnorm(length(ech),mean=0,sd=0.001),"pnorm",mean(ech),sqrt(var(ech)))
+
+# COMPLEMENTS sur le test de Kolmogorov : la statistique de test D correspond au "sup" de l'écart entre la
+# fonction de répartition empirique de l'échantillon et celle de la loi normale ayant une moyenne égale 
+# à celle de l'échantillon et un écart-type égal à celui de l'échantillon.
+# Si l'échantillon est bien issu d'une loi normale, la valeur de D devrait être petite.
+# Le test rejette donc l'hypothèse H0 dès que la statistique de test D dépasse un certain seuil (qui a été
+# calculé théoriquement par Kolmogorov).
+# Les deux lignes de codes R ci-dessous permettent de tracer ces deux fonctions de répartition :
+
+par(mfrow=c(1,1))
+plot.ecdf(joie)  # tracé de la fonction de répartition empirique de l'échantillon (en noir)
+plot.ecdf(rnorm(5000,mean=mean(joie),sd=sqrt(var(joie))),add=T,lty="dotted",pch=" ",col=2) # tracé de la
+# fonction de répartition de la loi normale la mieux adaptée (en rouge).
+# NB : ecdf = "empirical cumulative distribution function" = fonction de répartition empirique.
+# On observe bien que ces deux courbes sont relativement proches l'une de l'autre, d'où le non-rejet de H0.
+# COMMENTAIRES pour la suite de l'exercice :
+#===========================================
+# Une fois la normalité acceptée, passons maintenant au test de Student.
+# On désire tester si H0 : mu=990 (vitesse de Cornu) contre une hypothèse alternative qu'il convient de
+# choisir convenable !
+# Au des observations faites précédemment (vitesse moyenne observée par Michelson = 909, décalage de la 
+# densité vers la gauche par rapport à 990), il parait raisonnable de choisir comme alternative 
+# H1 : mu<990 (plutôt que l'hypothèse H1 par défaut qui est "mu différent de 990").
+# Cela revient à repondre à la question "est-ce que la vitesse moyenne mu de Michelson est
+# significativement plus petite que la vitesse 909 proposée par Cornu ?".
+# NB : cela ne sert à rien de mettre une partie du risque de première espèce alpha sur la question "est-ce
+# la vitesse moyenne mu de Michelson est significativement plus grande que la vitesse 909 proposée 
+# par Cornu ?". En règle générale, évitons de mettre du risque là où il n'y a pas de raison d'en mettre.
+# 
+# Pour tester H0 : mu=990 contre H1 : mu <990, il faut taper la commende :
+
+t.test(joie,mu=990,alternative="less")
+
+# Commentaires : pour lire les sorties de ce test
+# On retrouve de bas en haut l'estimation ponctuelle de mu, l'estimation par intervalle de confiance de mu 
+# avec un niveau de confiance à 95% (par défaut), puis le test de Student de H0 versus H1.
+#
+#	One Sample t-test
+#
+# data:  ech
+# t = -3.4524, df = 19, p-value = 0.001334   <-- test de Student de H0 : mu=990 contre H1 : mu<990
+# alternative hypothesis: true mean is less than 990
+# 95 percent confidence interval:  <--- estimation par intervalle de confiance de mu
+#      -Inf 949.5692
+# sample estimates:       <--- estimation ponctuelle de mu
+# mean of x 
+#       909 
+#
+# NB1 : pour l'intervalle de confiance, lorsque l'hypothèse alternative H1 testée est unilatérale, 
+# l'intervalle de confiance ne fournit qu'une borne supérieure si H1 : mu < mu0
+# ou qu'une borne supérieure si H1 : mu>mu0.
+# Interprétation de l'intervalle de confiance obtenu ici (de manière un peu abusive car il s'agit de la
+# réalisation de l'intevalle de confiance théorique qui est un objet aléatoire) : "Il y a 95% de chances 
+# que la vitesse moyenne de la lumière issu des expériences de Michelson soit inférieure à 949.57 (<990)".
+#
+# NB2 : pour le test de Student : "t" correspond à la réalisation de la statistique Tn du test de Student
+# vu en cours, "df" correspond au nombre de degrés de liberté de la loi de Student que suit la statistique
+# Tn sous H0, à savoir la loi de Student T(n-1). Au vu de la valeur de la statistique de test Tn, de sa loi
+# sous H0 (T(n-1)) et de l'hypothèse H1 (qui donne la forme de la région de rejet), la "p-value" du test 
+# a pu être calculée.
+# Interprétation : ici p-value=0.13% < 5%(= risque de première espèce que l'on se donne), on rejette donc
+# l'hypothèse nulle H0 : mu=990 et l'on retient l'hypothèse alternative H1 : mu<990.
+# Il y a donc une différencen significative entre la vitesse moyenne de la lumière obtenue par Michelson
+# et celle proposée par Cornu. Il va donc y avoir débat en l'académie de sciences française et son
+# homologue américaine !
+
+# COMPLEMENTS :
+# 1) Par défaut, si on ne précise pas l'alternative, le tets de Student réalisé est le suivant :
+# H0 : mu=990 contre H1 : mu différent de 990, voir ci-dessous.
+# Il s'agit d'une alternative bilatérale.
+# L'intervalle de confiance de mu à 95% est alors fourni avec deux bornes en répartissant 2,5% du risque
+# de chaque côté.
+
+t.test(joie,mu=990)
+
+#	One Sample t-test
+#
+# data:  ech
+# t = -3.4524, df = 19, p-value = 0.002669
+# alternative hypothesis: true mean is not equal to 990
+# 95 percent confidence interval:
+# 859.8931 958.1069
+# sample estimates:
+# mean of x 
+#       909 
+#
+# Commentaires sur les sorties numériques de ce test : 
+#-----------------------------------------------------
+# En ayant mis 2,5% du risque pour mu>990 (inutile ici) 
+# et 2,5% pour mu<990, on voit que la p-value du test est 0.27%<5% donc on rejette toujours H0, 
+# mais la p-value a augmenté par celle obtenue précédemment (p-value=0.13%) 
+# avec H1 : mu<990 (seule alternative utile).
+#
+# 2) Si on met l'alternative H1 :mu>990 (alternative absurde ici vu que l'on a observé une moyenne ( égale à 
+# 909) bien inférieure à 990), on obtient les sorties numériques suivantes :
+
+t.test(joie,mu=990,alternative="greater")
+
+#	One Sample t-test
+#
+# data:  ech
+# t = -3.4524, df = 19, p-value = 0.9987
+# alternative hypothesis: true mean is greater than 990
+# 95 percent confidence interval:
+#  868.4308      Inf
+# sample estimates:
+# mean of x 
+#       909 
+#
+# Commentaires sur les sorties numériques de ce test : 
+#-----------------------------------------------------
+# On a ici une p-value égale à 99.87%>5%, ainsi on ne peut pas rejeter HO.
+# On conserve clairement H0 : mu=990 par défaut ! En effet l'alternative H1 est ici pire comme hypothèse
+# que H0. On sait bien que mu est significativement inférieur à 990 !
+# C'est pour cela qu'il faut toujours bien réfléchir au choix de l'hypothèse alternative H1 et que
+# l'étude descriptive préliminaire (statistiques descriptives, graphiques) est très utile.
+
+# Question d) Donner un intervalle de confiance de niveau 95% (pui de niveau 90%) du paramètre mu.
+#-------------------------------------------------------------------------------------------------
+#
+# Si désire un intervalle de confiance bilatéral, il convient de ne pas spécifier l'alternative dans
+# la fonction "t.test" (par défaut alternative="two.sided", c'est à dire "différent").
+# Il est possible de récupérer uniquement les bornes de l'invervalle de confiance de mu en utilisant
+# "$conf.int" et de gérer le niveau de confiance avec le paramètre "conf.level", par défaut 95%) :
+
+t.test(ech,mu=990,conf.level=0.95)$conf.int  # IC (bilatéral) de mu à 95%
+t.test(ech,mu=990,conf.level=0.90)$conf.int  # IC (bilatéral) de mu à 90%
+
+# Commentaires sur ces deux intervalles de confiance de mu :
+# L'intervalle de confiance de mu à 90% est naturellement plus étroit que celui à 95%, en effet on
+# s'autorise 5% d'erreur supplémentaire.
+# Pour l'interprétation de l'IC de mu à 95%, on peut dire (abusivement, voir une remarque précédente) 
+# qu'il y a 95% de chances que la vitesse moyenne de la lumière obtenue par Michelson
+# soit comprise entre 859.8931 et 958.1069 (en rajoutant 299.000).
+
+## TEST POUR PERCEPTION OF CORRUPTION ##
+
+
+
                                                                                 #####  ANALYSE EN COMPOSANTES PRINCIPALES (ACP) ##### 
 
 #----------------------------------------
